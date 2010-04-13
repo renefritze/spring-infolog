ce60072232d3d568c74e07698179ed60bce3720f
Zydox <root@Omoikane.(none)>
2010-04-13 22:01:23 +0200

    Infolog.txt pharser & updated crash table

--- backend.py
+++ backend.py
@@ -3,19 +3,37 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import *
 import datetime, os
+import re
 
 current_db_rev = 5
 Base = declarative_base()
 
 class Crash(Base):
-	__tablename__ 	= 'records'
-	id 				= Column( Integer, primary_key=True,index=True )
-	date 			= Column( DateTime )
-	extensions		= Column( PickleType )
-	settings		= Column( PickleType )
-	script			= Column( PickleType )
-	filename		= Column( String(255) )
-	platform		= Column( String(100) )
+	__tablename__ 			= 'records'
+	id 						= Column( Integer, primary_key=True,index=True )
+	date 					= Column( DateTime )
+	extensions				= Column( PickleType )
+	settings				= Column( PickleType )
+	script					= Column( PickleType )
+	filename				= Column( String(255) )
+	platform				= Column( String(100) )
+	spring					= Column( String(100) )
+	map						= Column( String(100) )
+	mod						= Column( String(100) )
+	gameid					= Column( String(100) )
+	sdl_version				= Column( String(100) )
+	glew_version			= Column( String(100) )
+	al_vendor				= Column( String(100) )
+	al_version				= Column( String(100) )
+	al_renderer				= Column( String(100) )
+	al_extensions			= Column( String(100) )
+	alc_extensions			= Column( String(100) )
+	al_device				= Column( String(100) )
+	al_available_devices	= Column( String(255) )
+	gl_version				= Column( String(100) )
+	gl_vendor				= Column( String(100) )
+	gl_renderer				= Column( String(100) )
+	
 
 	important_settings = ['Shadows']
 	
@@ -119,9 +137,67 @@ class Backend:
 		if data.has_key( 'platform.txt' ):
 			crash.platform = data['platform.txt'].strip()
 		crash.status = None
-		#insert actual parsing here
+		
+		al_available_devices = []
+		for line in data['infolog.txt'].splitlines ():
+			match = re.search ('^Spring*(/d*.)*', line)
+			if (match):
+				crash.spring = line
+			value = self.parseInfologSub ('^[\[ 0\]]*Using map[ ]*', line)
+			if (value):
+				crash.map = value
+			value = self.parseInfologSub ('^[\[ 0\]]*Using mod[ ]*', line)
+			if (value):
+				crash.mod = value
+			value = self.parseInfologSub ('^[\[ 0\]]*GameID:[ ]*', line)
+			if (value):
+				self.gameid = value
+			value = self.parseInfologSub ('^[\[ 0\]]*SDL:[ ]*', line)
+			if (value):
+				crash.sdl_version = value
+			value = self.parseInfologSub ('^[\[ 0\]]*GLEW:[ ]*', line)
+			if (value):
+				crash.glew_version = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*Vendor:[ ]*', line)
+			if (value):
+				crash.al_vendor = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*Version:[ ]*', line)
+			if (value):
+				crash.al_version = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*Renderer:[ ]*', line)
+			if (value):
+				crash.al_renderer = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*AL Extensions:[ ]*', line)
+			if (value):
+				crash.al_extensions = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*ALC Extensions:[ ]*', line)
+			if (value):
+				crash.alc_extensions = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]*Device:[ ]*', line)
+			if (value):
+				crash.al_device = value
+			value = self.parseInfologSub ('^[\[ 0-9\]]*Sound:[ ]{23}', line)
+			if (value):
+				al_available_devices.append (value)
+			value = self.parseInfologSub ('^[\[ 0-9\]]*GL:[ ]*', line)
+			if (value):
+				if (not crash.gl_version):
+					crash.gl_version = value
+				if (not crash.gl_vendor):
+					crash.gl_vendor = value
+				if (not crash.gl_renderer):
+					crash.gl_renderer = value
+			
+		if (al_available_devices):
+			crash.al_available_devices = "\n".join (al_available_devices)
 		
 		session.add( crash )
 		session.commit()
 		session.close()
 		return crash_id
+	
+	
+	def parseInfologSub (self, preg, line):
+		match = re.search (preg, line)
+		if (match):
+			return (line.replace (match.group (0), ''))
