7012f2a104cfdc28c7f9e6e3e56cbd100883f94b
Zydox <root@Omoikane.(none)>
2010-06-12 00:20:40 +0200

    PHP: Added mimedata to the download page

--- php/pages/DownloadFile.php
+++ php/pages/DownloadFile.php
@@ -2,7 +2,12 @@
 if (file_exists ("../" . $Post['File']))	{
 	$MySQL_Result = DB_Query ("SELECT id FROM records WHERE filename='" . mysql_escape_string ($Post['File']) . "'");
 	if (mysql_num_rows ($MySQL_Result))	{
-		$FP = fopen ("../" . $Post['File'], "r");
+		$File = "../" . $Post['File'];
+	    header("Content-Type: application/octet-stream");
+    	header("Content-Length: " . filesize ($File));
+	    header("Content-Disposition: attachment; filename=" . $Post['File']);
+    	header("Content-Transfer-Encoding: binary");
+		$FP = fopen ($File, "r");
 		fpassthru ($FP);
 		fclose ($FP);
 	}
